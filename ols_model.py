f12713dddd371fb7212582bc5820314420874772
Mukesh Kumar Sharma <mks.mukesh1996@gmail.com>
2022-02-21 06:12:40 +0530

    first commit

---
+++ ols_model.py
@@ -0,0 +1,19 @@
+import statsmodels.api as sm
+import pandas as pd
+
+# reading data from the csv
+data = pd.read_csv('train.csv')
+
+# defining the variables
+x = data['x'].tolist()
+y = data['y'].tolist()
+
+# adding the constant term
+x = sm.add_constant(x)
+
+# performing the regression
+# and fitting the model
+result = sm.OLS(y, x).fit()
+
+# printing the summary table
+print(result.summary())
